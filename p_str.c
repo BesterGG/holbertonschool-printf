d8b599ac818ec781af041875a8f29b045aeef2bb
Jose Alvarez <5135@holbertonschool.com>
2022-07-08 08:40:08 -0700

    str punterio

--- p_str.c
+++ p_str.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <stdlib.h>
+
 int p_str(va_list args)
 {
-	char str = va_arg(args, char);
+	char *str = va_arg(args, char);
 	int i = 0;
 
 	while (str[i] != '\0' && str != NULL)
